f51e8b3454048a37fb279adee51595553d228b89
jinseok0212 <qowlstjr123@icloud.com>
2025-05-12 17:23:32 +0900

    클래스, 모듈 등 이해

---
+++ python/test.py
@@ -0,0 +1,23 @@
+from practice import cal
+
+a = cal(4, 7)
+
+print(a.add())
+
+
+'''
+game/
+    __init__.py
+    sound/
+        __init__.py
+        echo.py
+        wav.py
+    graphic/
+        __init__.py
+        screen.py
+        render.py
+    play/
+        __init__.py
+        run.py
+        test.py
+'''
