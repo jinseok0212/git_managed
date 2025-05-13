348dce8336ab9714d0b38b07bf86799671e4f79a
jinseok0212 <qowlstjr123@icloud.com>
2025-05-13 16:15:01 +0900

    admin에서 유저조회

--- self_django/users/admin.py
+++ self_django/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('추가정보', {'fields': ('nickname', 'age')}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('추가정보', {'fields': ('nickname', 'age')}),
+    )
+    
+    list_display = ['username', 'email', 'nickname', 'age']
+
+admin.site.register(CustomUser, CustomUserAdmin)
