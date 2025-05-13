27e6f83a24cc681d78abe6806bd42415df1eb3f1
jinseok0212 <qowlstjr123@icloud.com>
2025-05-13 14:33:54 +0900

    user 모델 커스터마이징 - nickname,age 추가

--- self_django/users/models.py
+++ self_django/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=30,blank = True)
+    age = models.IntegerField(null= True, blank = True)
+
+    def __self__(self):
+        return self.username
