801bcfc4bb04f45406b5e946762b00aee03ace00
jinseok0212 <qowlstjr123@icloud.com>
2025-05-21 13:41:11 +0900

    루트에 보드 앱 url 연결

--- self_django/self_project/urls.py
+++ self_django/self_project/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('', include('users.urls')), # 본 프젝에 연결
+    path('posts/', include('board.urls')), 
 ]
